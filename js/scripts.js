b287d2c0432e63b01e59bf3ea32406537c664003
Nicholas Reeder <nickreeder32@gmail.com>
2021-03-24 11:42:52 -0700

    add jquery functions for form submission
    co-authored-by: Carlos Mendez <carlosmendez86@gmail.com>
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,12 @@ function divide(number1, number2) {
   return number1 / number2;
 }
 
+$(document).ready(function () {
+  $("form#add").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
